5b1a9b05a925820e700defebd353561422a4ed98
rajarshidas <rajdasr@amazon.com>
2020-08-15 05:47:39 +0000

    Updated php app with showing host name and IP

--- flask-docker-app/myapp/index.php
+++ flask-docker-app/myapp/index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Example Title</title>
+        <title>Sysdig Demo</title>
     </head>
     <body>
         <p>
             Hello. Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
+    
+    <?php 
+    
+    // PHP program to obtain IP address of 
+    // the server 
+    
+    // Creating a variable to store the 
+    // server address 
+    $hostname = gethostname();
+    $ip_server = $_SERVER['SERVER_ADDR']; 
+    
+    // Printing the stored address 
+    echo "Hostname is: $hostname. Host IP is: $ip_server"; 
+    
+    ?> 
+
 </html>
